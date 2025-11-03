b7197a6ca81784dcb2f6d15254668babcc8596c9
Phcode007 <paulobelarmino047@gmail.com>
2025-11-03 15:23:45 -0300

    Navbar, Footer e Home criadas

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 flex gap-4">
+      <Link to="/">Home</Link>
+      <Link to="/categorias">Categorias</Link>
+      <Link to="/categorias/form">Nova Categoria</Link>
+    </nav>
+  );
+}
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Bem-vindo à Fast Farma 💊</h1>
+      <p>Gerencie suas categorias e produtos rapidinho!</p>
+    </div>
+  );
+}
