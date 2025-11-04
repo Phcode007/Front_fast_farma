f70cfe4b34d8133b81986556aa8c79e60aa555cf
Phcode007 <paulobelarmino047@gmail.com>
2025-11-03 22:57:10 -0300

    Rotas Implementadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
 function App() {
-  return;
-  <>
-    <Navbar />
-    <Home />
-    <Footer />
-  </>;
+  return (
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            {/* Adicione mais rotas aqui depois */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,9 @@ export default function Navbar() {
   return (
     <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
       <div className="container flex justify-between text-lg mx-8">
-        Fast Farma
+        <Link to="/home" className="text-2xl font-bold">
+          Fast Farma
+        </Link>
         <div className="flex gap-4">
           <Link to="/">Home</Link>
           <Link to="/categorias">Categorias</Link>
