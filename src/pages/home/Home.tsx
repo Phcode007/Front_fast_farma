90453d0e4a92411f36a1eddf508c96078e61881c
Phcode007 <paulobelarmino047@gmail.com>
2025-11-06 15:09:03 -0300

    Att logo

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,35 +1,28 @@
-import ListaCategorias from "../../components/categoria/listacategorias/ListaCategorias"
+import ListaCategorias from "../../components/categoria/listacategorias/ListaCategorias";
 
 function Home() {
   return (
     <>
       <div className="bg-[#8FABD4] flex justify-center">
         <div className="container grid grid-cols-2 text-white">
-
           <div className="flex flex-col gap-4 items-center justify-center py4">
-            
-            <h2 className="text-5xl font-bold">
-              Farmacia Gen
-            </h2>
+            <h2 className="text-5xl font-bold">Fast Farm</h2>
 
-            <p className="text-xl">
-              Saúde e Bem-Estar para todos!
-            </p>
+            <p className="text-xl">Saúde e Bem-Estar para todos!</p>
           </div>
 
           <div className="flex justify-center">
             <img
-              src="https://i.imgur.com/1W500uH.png"
-              alt="Imagem Página Home"
+              src="https://imgur.com/a/8LpneQR"
+              alt="Logo Fast Farma"
               className="w-2/3 rounded-3xl"
             />
           </div>
-
         </div>
       </div>
       <ListaCategorias />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
