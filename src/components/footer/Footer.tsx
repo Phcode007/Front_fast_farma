02634b0520bd916ffeebfef9b9597ba8dfb63c40
Phcode007 <paulobelarmino047@gmail.com>
2025-11-06 16:06:51 -0300

    .

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,23 +1,16 @@
+/* eslint-disable prefer-const */
 function Footer() {
-
-  let data = new Date().getFullYear()
-
+  let data = new Date().getFullYear();
 
   return (
     <div className="flex justify-center bg-[#4A70A9] text-white">
-
       <div className="container flex flex-col items-center py-4">
-        <p className="text-xl font-bold">
-          Projeto Farmacia | Copyright: {data}
-        </p>
+        <p className="text-xl font-bold">Fast Farma| Copyright: {data}</p>
 
-        <p className="text-lg">
-          &copy; Feito por Elzilane Barreto 
-        </p>
+        <p className="text-lg">&copy; Feito por Paulo Henrique Belarmino</p>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div
-        className="w-full flex justify-center py-4 bg-[#4A70A9] text-white"
-      >
-        <div className="container flex justify-between text-lg mx-8">
-          <Link to="/home" className="hover:underline text-2xl font-bold">
-          Projeto Farmacia
-          </Link>
+    <div className="w-full flex justify-center py-4 bg-[#4A70A9] text-white">
+      <div className="container flex justify-between text-lg mx-8">
+        <Link to="/home" className="hover:underline text-2xl font-bold">
+          Fast Farma 💊
+        </Link>
 
-          <div className="flex gap-4">
-            <Link to="/categorias" className="hover:underline">Listar Categorias</Link>
-            <Link to="/cadastrarcategoria" className="hover:underline">Cadastrar Categoria</Link>
-            {/* Sair */}
-          </div>
+        <div className="flex gap-4">
+          <Link to="/categorias" className="hover:underline">
+            Listar Categorias
+          </Link>
+          <Link to="/cadastrarcategoria" className="hover:underline">
+            Cadastrar Categoria
+          </Link>
+          {/* Sair */}
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
